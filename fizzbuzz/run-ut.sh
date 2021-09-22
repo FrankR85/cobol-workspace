9896eb976e4460b1fca6f5636c25ac08641290da
Frank Rehfeldt <frank@Franks-MacBook-Air.local>
2021-09-22 12:36:41 +0200

    erster Testcase

--- fizzbuzz/run-ut.sh
+++ fizzbuzz/run-ut.sh
@@ -1,3 +1,3 @@
-/home/cobol/cobol-unit-test/run-ut -d -p fizzbuzz FIZZBUZC FIZZBUZZ FIZZBUZT
+/home/cobol/cobol-unit-test/run-ut -p fizzbuzz FIZZBUZC FIZZBUZZ FIZZBUZT
 
 #cobc -x -std=ibm /vagrant/cobol/fizzbuzz/src/FIZZBUZZ.CBL -o /vagrant/cobol/fizzbuzz/target/FIZZBUZZ
